7025ba1d26a6f6892d4b857ba9eb2ef284391c80
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2026-08-02 12:57:38 +0530

    fix server tsconfig and env typing

---
+++ server/drizzle.config.ts
@@ -0,0 +1,12 @@
+// import 'dotenv/config';
+// import process from 'node:process';
+// import { defineConfig } from 'drizzle-kit';
+
+// export default defineConfig({
+//   dialect: 'postgresql', // options: 'postgresql' | 'mysql' | 'sqlite'
+//   schema: './src/db/schema.ts',
+//   out: './drizzle',
+//   dbCredentials: {
+//     url: process.env.DATABASE_URL!,
+//   },
+// });
